37755ff66d252aa6b89ce7778bf452a09a44e64b
Nguyen Quang An <ngquangan@gmail.com>
2019-07-20 16:26:45 +0700

    Custom row column

--- fl06_row_column/lib/main.dart
+++ fl06_row_column/lib/main.dart
@@ -15,6 +15,29 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("Flutter App"),
         ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                width: 120,
+                height: 120,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.amberAccent,
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                color: Colors.green,
+              )
+            ],
+          ),
+        )
       ),
     );
   }
