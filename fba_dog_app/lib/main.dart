b67a3ddebf788f549f4f5b21d8bd9e5e47d64885
An Nguyen Quang <ngquangan@gmail.com>
2019-08-07 11:22:44 +0700

    Create list dog card and create gradient background

--- fba_dog_app/lib/main.dart
+++ fba_dog_app/lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 import 'dog_card.dart';
+import 'dog_list.dart';
 import 'dog_model.dart';
 
 void main() => runApp(MyApp());
@@ -49,7 +50,27 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.black87,
       ),
       body: Container(
-        child: DogCard(initialDoggos[1]), // New code
+        // Add box decoration
+        decoration: BoxDecoration(
+          // Box decoration takes a gradient
+          gradient: LinearGradient(
+            // Where the linear gradient begins and ends
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            // Add one stop for each color. Stops should increase from 0 to 1
+            stops: [0.1, 0.5, 0.7, 0.9],
+            colors: [
+              // Colors are easy thanks to Flutter's Colors class.
+              Colors.indigo[800],
+              Colors.indigo[700],
+              Colors.indigo[600],
+              Colors.indigo[400],
+            ],
+          ),
+        ),
+        child: Center(
+          child: DogList(initialDoggos),
+        ),
       ),
     );
   }
