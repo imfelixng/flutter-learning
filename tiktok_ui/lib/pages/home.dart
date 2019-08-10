2cd579ec909410edc40d3cec5af16070808e0be5
An Nguyen Quang <ngquangan@gmail.com>
2019-08-10 14:39:13 +0700

    fix home

--- tiktok_ui/lib/pages/home.dart
+++ tiktok_ui/lib/pages/home.dart
@@ -10,40 +10,16 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Widget get topSection => Container(
-    height: 100.0,
-    padding: EdgeInsets.only(bottom: 15.0),
-    alignment: Alignment(0.0, 1.0),
-    child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text('Following'),
-          Container(
-            width: 15.0,
-          ),
-          Text('For you',
-              style: TextStyle(
-                  fontSize: 17.0, fontWeight: FontWeight.bold))
-        ]),
-  );
+        height: 100.0,
+        padding: EdgeInsets.only(bottom: 15.0),
+        color: Colors.yellow[300],
+      );
 
   Widget get middleSection => Expanded(
       child: Row(
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[VideoDescription(), ActionsToolbar()]));
-
-  Widget get videoDescription =>
-      Expanded(
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(height: 10.0, color: Colors.green[300], margin: EdgeInsets.only(top: 10)),
-              Container(height: 10.0, color: Colors.green[300], margin: EdgeInsets.only(top: 10)),
-              Container(height: 10.0, color: Colors.green[300], margin: EdgeInsets.only(top: 10))
-            ]),
-      );
 
   @override
   Widget build(BuildContext context) {
