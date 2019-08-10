8646e47a517c428c29e8bebded7aa1cf767ba75d
An Nguyen Quang <ngquangan@gmail.com>
2019-08-11 02:01:10 +0700

    Add stream builder for handle async list

--- bloc_app/lib/home.dart
+++ bloc_app/lib/home.dart
@@ -1,47 +1,63 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+import 'home_model.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  HomeModel model = HomeModel();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-            // We want to refresh, but this actually does nothing. Which is the limitation of future builder
-             _getListData();
-          },
-          child: Icon(
-            Icons.refresh,
-            color: Colors.white,
-        ),
-        ),
-        backgroundColor: Colors.grey[900],
-        body: FutureBuilder(
-          future: _getListData(hasError: false, hasData: true),
-          builder: (buildContext, snapshot) {
-
-            if (snapshot.hasError) {
-              return _getInformationMessage(snapshot.error);
-            }
-
-            if(!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator());
-            }
-
-            var listItems = snapshot.data;
-
-            // When empty data is returned
-            if(listItems.length == 0) {
-              return _getInformationMessage('No data found for your account. Add something and check back.');
-            }
-
-            return ListView.builder(
-                itemCount: listItems.length,
-                itemBuilder: (buildContext, index) => _getListItemUi(index, listItems));
-          },
-        )
-    );
+  void initState() {
+    model.getListData();
+    super.initState();
   }
+
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//        floatingActionButton: FloatingActionButton(
+//            onPressed: () {
+//            // We want to refresh, but this actually does nothing. Which is the limitation of future builder
+//             _getListData();
+//          },
+//          child: Icon(
+//            Icons.refresh,
+//            color: Colors.white,
+//        ),
+//        ),
+//        backgroundColor: Colors.grey[900],
+//        body: FutureBuilder(
+//          future: _getListData(hasError: false, hasData: true),
+//          builder: (buildContext, snapshot) {
+//
+//            if (snapshot.hasError) {
+//              return _getInformationMessage(snapshot.error);
+//            }
+//
+//            if(!snapshot.hasData) {
+//              return Center(child: CircularProgressIndicator());
+//            }
+//
+//            var listItems = snapshot.data;
+//
+//            // When empty data is returned
+//            if(listItems.length == 0) {
+//              return _getInformationMessage('No data found for your account. Add something and check back.');
+//            }
+//
+//            return ListView.builder(
+//                itemCount: listItems.length,
+//                itemBuilder: (buildContext, index) => _getListItemUi(index, listItems));
+//          },
+//        )
+//    );
+//  }
 
   Widget _getListItemUi(int index, List<String> listItems) {
     return Container(
@@ -69,20 +85,57 @@ class Home extends StatelessWidget {
         ));
   }
 
-  Future<List<String>> _getListData({
-    bool hasError = false,
-    bool hasData = true}) async {
-    await Future.delayed(Duration(seconds: 2));
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        // We want to refresh, but this actually does nothing. Which is the limitation
+        model.getListData();
+      },
+      child: Icon(
+        Icons.refresh,
+        color: Colors.white,
+      ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+                height: 50,
+                width: double.infinity,
+                color: Colors.green,
+                child: Text('Demo Text')),
+            Expanded(
+              child: StreamBuilder(
+                  stream: model.homeState,
+                  builder: (buildContext, snapshot) {
 
-    if(hasError) {
-      return Future.error('An error occurred while fetching the data. Please try again later.');
-    }
+                    if(snapshot.hasError) {
+                      return _getInformationMessage(snapshot.error);
+                    }
 
-    if(!hasData) {
-      return List<String>();
-    }
+                    // Use busy indicator if there's no state yet, and when it's busy
+                    if (!snapshot.hasData || snapshot.data == HomeViewState.Busy) {
+                      return Center(child: CircularProgressIndicator());
+                    }
 
-    return List<String>.generate(100, (index) => '$index title');
+                    // use explicit state instead of checking the lenght
+                    if(snapshot.data == HomeViewState.NoData) {
+                      return _getInformationMessage('No data found for your account. Add something and check back.');
+                    }
+
+
+                    return ListView.builder(
+                        itemCount: model.lists.length,
+                        itemBuilder: (buildContext, index) => _getListItemUi(index, model.lists));
+                  }
+
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
-
 }
+
