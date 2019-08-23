648660274df46ff918514c59a74762ff31071248
An Nguyen Quang <ngquangan@gmail.com>
2019-08-23 14:11:53 +0700

    Finish provider v3 app

--- provider_v3_app/lib/main.dart
+++ provider_v3_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:provider_arc/ui/router.dart';
 
 import 'core/constants/app_contstants.dart';
+import 'provider_setup.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [],
+      providers: providers,
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
