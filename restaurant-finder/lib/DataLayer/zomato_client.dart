e294226fcd16eb9d8b863ca3e82f51c01d76ce49
An Nguyen Quang <ngquangan@gmail.com>
2019-09-11 12:05:14 +0700

    Finish restaurant finder app

--- restaurant-finder/lib/DataLayer/zomato_client.dart
+++ restaurant-finder/lib/DataLayer/zomato_client.dart
@@ -38,7 +38,7 @@ import 'location.dart';
 import 'restaurant.dart';
 
 class ZomatoClient {
-  final _apiKey = 'PASTE YOUR API KEY HERE';
+  final _apiKey = '5ea6910d79ed78e5f3882fddf76671ce';
   final _host = 'developers.zomato.com';
   final _contextRoot = 'api/v2.1';
 
